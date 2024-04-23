9fa4040913cd7f9a86447fd206a14af6359df9b4
Bryan Littlefield <bryan@builtmighty.com>
2024-04-23 16:10:44 +0000

    Fix URLs in shell scripts to use --allow-root flag
    Signed-off-by: GitHub <noreply@github.com>

--- scripts/wp-update-url.sh
+++ scripts/wp-update-url.sh
@@ -4,21 +4,21 @@ wp --path=${local_web_root} config set DB_NAME ${import_db_name} --allow-root --
 wp --path=${local_web_root} config set DB_USER ${import_db_user}  --allow-root --url=%
 wp --path=${local_web_root} config set DB_PASSWORD ${import_db_pass}  --allow-root --url=%
 wp --path=${local_web_root} config set DB_HOST ${import_db_host} --allow-root --url=%
-wp --path=${local_web_root} config delete WP_HOME  --allow-root --url=%
-wp --path=${local_web_root} config delete WP_SITEURL  --allow-root --url=%
+wp --path=${local_web_root} config set WP_HOME  https://${local_domain} --allow-root --url=%
+wp --path=${local_web_root} config set WP_SITEURL  https://${local_domain} --allow-root --url=%
 
 # Check if the site is a Multi-site install or not, then run search and replace of the old domain to new
-if $(wp --path=${local_web_root} --url=${remote_domain} core is-installed --network); then
+if $(wp --path=${local_web_root} --url=${remote_domain} core is-installed --network --allow-root); then
     echo "Multisite Install FOUND for ${remote_domain}, replacing site URL..";
-    wp search-replace --path=${local_web_root} --url=${remote_domain} ${remote_domain} ${local_domain} --recurse-objects --network --skip-columns=guid --skip-tables=wp_users --all-tables --url=%
-    if [ $(wp --path=${local_web_root} config get 'DOMAIN_CURRENT_SITE') = ${remote_domain} ]
+    wp search-replace --path=${local_web_root} --url=${remote_domain} ${remote_domain} ${local_domain} --recurse-objects --network --skip-columns=guid --skip-tables=wp_users --all-tables --allow-root --url=%
+    if [ $(wp --path=${local_web_root} config get 'DOMAIN_CURRENT_SITE' --allow-root) = ${remote_domain} ]
     then
         echo "DOMAIN_CURRENT_SITE matches set remote_domain to replace. Updating DOMAIN_CURRENT_SITE in wp-config.php ..";
         wp --path=${local_web_root} config set DOMAIN_CURRENT_SITE ${local_domain} --allow-root
     fi
 else
     echo "Multisite Install NOT found for ${remote_domain}, replacing site URL for Single Site WP Install..";
-    wp search-replace --path=${local_web_root} ${remote_domain} ${local_domain} --recurse-objects --skip-columns=guid --skip-tables=wp_users --all-tables --url=%
+    wp search-replace --path=${local_web_root} ${remote_domain} ${local_domain} --recurse-objects --skip-columns=guid --skip-tables=wp_users --all-tables --allow-root --url=%
 fi
 
 # wp --path=${local_web_root} search-replace ${remote_domain} ${local_domain} --skip-columns=guid --allow-root;
